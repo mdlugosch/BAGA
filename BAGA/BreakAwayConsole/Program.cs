7e7a26d94f82b24af7e1e273302059527ba74288
Michael Dlugosch <michael.dlugosch@web.de>
2017-11-30 16:28:20 +0100

    EF Tutorial

---
+++ BAGA/BreakAwayConsole/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using DataAccess;
+
+namespace BreakAwayConsole
+{
+    class Program
+    {
+        private static void InsertDestination()
+        {
+            var destination = new Destination()
+            {
+                Country = "Indonesia",
+                Description = "EcoTourism at its best in exquisite Bali",
+                Name = "Bali"
+            };
+
+            using (var context = new BreakAwayContext())
+            {
+                context.Destinations.Add(destination);
+                context.SaveChanges();
+            }
+        }
+
+        public static void ShowDestination()
+        {
+            using (var context = new BreakAwayContext())
+            {
+                var query = from row in context.Destinations
+                            select row;
+
+                foreach (var element in query)
+                {
+                    Console.WriteLine(element.DestinationId + " / " + element.Name + " / " + element.Description);
+                }
+            }
+        }
+
+        public static void ClearDestinations()
+        {
+            using (var context = new BreakAwayContext())
+            {
+                var query = from row in context.Destinations
+                            where row.DestinationId>0
+                            select row;
+
+                foreach (var element in query)
+                {
+                    context.Destinations.Remove(element);
+                }
+                context.SaveChanges();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            //InsertDestination();
+            //ClearDestinations();
+            ShowDestination();
+            Console.ReadKey();
+        }
+    }
+}
